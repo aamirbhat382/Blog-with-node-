52bb66f9875b20ad181b51793552c124cb527ab0
Aamir Bhat <aamirbhat382@gmail.com>
2021-01-08 18:48:25 +0530

    new layout

--- app/http/Controllers/home/articleController.js
+++ app/http/Controllers/home/articleController.js
@@ -1,3 +1,4 @@
+const e = require('express')
 const Article = require('../../../models/airtical')
     // const comment = require('../../../models/comment')
 const Comment = require('../../../models/comment')
@@ -6,16 +7,24 @@ function articleController() {
     return {
         showOne: async(req, res) => {
             const article = await Article.findOne({ slug: req.params.slug })
-            if (article == null) res.redirect('/')
-                // const latest_article = await Article.find({})
-            const comment = await Comment.find({ articleSlug: req.params.slug }, null, { sort: { 'createdAt': -1 } }).populate('customerId', '-password').exec((err, comment) => {
-                if (req.xhr) {
-                    return res.json(comment)
-                } else {
+            try {
+                if (article == null) res.redirect('/')
+                const Allarticle = await Article.find({}, null, { sort: { 'createdAt': 1 } })
+                const comment = await Comment.find({ articleSlug: req.params.slug }, null, { sort: { 'createdAt': -1 } }).populate('customerId', '-password').exec((err, comment) => {
+                    if (req.xhr) {
+                        return res.json(comment)
+                    } else {
 
-                    return res.render('articles/show', { article: article }, )
-                }
-            })
+                        return res.render('articles/show', { article: article, Allarticle: Allarticle })
+                    }
+                })
+            } catch (e) {
+                req.flesh('error', 'Something Want To Wrong')
+                console.log(e)
+            }
+
+            // const latest_article = await Article.find({})
+
 
         }
     }
